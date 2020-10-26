8c3250036e7f05a3ff928a0176f6b8032a546eb1
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2020-10-26 13:38:31 -0400

    Added to signup page

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,2 +1,23 @@
 class CoffeesController < ApplicationController
+
+    def new 
+        @coffee = Coffee.new 
+        @coffee.build_brand 
+    end 
+
+    def create 
+        @coffee = Coffee.new(coffee_params)
+        @coffee.user_id = session[:user_id]
+        if @coffee.save 
+            redirect_to coffee_path(@coffee)
+        else 
+            render :new 
+    end 
+end 
+
+    private 
+
+    def coffee_params 
+        params.require(:coffee).permit(:flavor, :description, :brand_id, brand_attributes: [:name])
+    end 
 end
