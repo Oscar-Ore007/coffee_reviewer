c598b36e7901058a7f74a596ed7b97f42b148710
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2020-10-26 16:10:17 -0400

    created new view form for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
   post '/signup' => 'users#create'
 
   resources :reviews
-  resources :coffees
+  resources :coffees do 
+    resources :reviews, only: [:new, :index]
+  end 
+
+
   resources :brands
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
