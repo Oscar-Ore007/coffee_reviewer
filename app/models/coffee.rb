5462bfa054922687df7443de2aa6fc0243d9b4c7
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2020-10-26 13:50:05 -0400

    built brand_attributes method

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -3,5 +3,10 @@ class Coffee < ApplicationRecord
     belongs_to :user #creator of it 
     has_many :reviews 
     has_many :users, through: :reviews
-    accepts_nested_attributes_for :brand  
+
+    # accepts_nested_attributes_for :brand 
+    
+    def brand_attributes=(attributes)
+        brand = Brand.find_or_create_by(attributes) if !attributes[:name].empty?
+    end 
 end
